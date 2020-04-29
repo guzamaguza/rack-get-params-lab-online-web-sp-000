15daf67554a510c08e8602bbf60e64cbac1d2915
Mark A Guza <guzamaguza@gmail.com>
2020-04-29 00:45:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
@@ -13,6 +14,13 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      resp.write @@cart
+    elsif req.path.match(/add/)
+      add_item = req.params["item"]
+      if add_item in @@itms
+      @@cart << add_item
+      end
     else
       resp.write "Path Not Found"
     end
